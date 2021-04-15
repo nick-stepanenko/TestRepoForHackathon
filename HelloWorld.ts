8f45b3178336c4ee2875a82d4cd5af740e244452
nick-stepanenko <Nick.Stepanenko@trilogy.com>
2021-04-15 11:18:19 +0300

    hello worlds for test DevFlows2

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,5 +1,3 @@
 console.log('Hello World!');
-console.log('Hello World2!');
-console.log('Hello World4!');
-console.log('Hello World4!');
-console.log('Hello World4!');
+console.log('Hello World22!');
+//asdfsadfds
